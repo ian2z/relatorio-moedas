69a3af7371103893541e4f67449be11117ce9e1f
Josef Ian <josefian2016@gmail.com>
2025-06-28 19:55:45 -0300

    criando gerador do openpyxl + iniciando funcoes de formatação de registros das APIs

--- services/api_client.py
+++ services/api_client.py
@@ -1,7 +1,7 @@
 import requests #biblioteca em python que permite realizar requisições de APIs
-from datetime import datetime #bilioteca usada para calcular o tempo de lo
+from datetime import datetime #bilioteca de tempo para registrar os logs
 
-def get_json(url): #função que realiza uma requisição GET, através de uma URL, e também os erros gerados pela mesma requisição
+def getJson(url): #função que realiza uma requisição GET, através de uma URL, e também os erros gerados pela mesma requisição
     try:
         response = requests.get(url, timeout=5) #Realiza a requisição, em até no máximo 5 segundos
         response.raise_for_status() #Obtem o código da requisição: 200, 401, etc
@@ -10,20 +10,41 @@ def get_json(url): #função que realiza uma requisição GET, através de uma U
         return {"erro na requisição": str(e)}
 
 #funcões criadas para as requisições específicas
-def get_cotacoes():
+def getCotacoes():
     url = "https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL"
-    return get_json(url)
+    return getJson(url)
 
-def get_historico_bitcoin():
+def getHistoricoBitcoin():
     url = "https://economia.awesomeapi.com.br/json/daily/BTC-BRL/30"
-    return get_json(url)
+    return getJson(url)
 
-def get_taxa_selic():
-    url = "https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL"
-    return get_json(url)
+def getTaxaSelic():
+    url = "https://api.bcb.gov.br/dados/serie/bcdata.sgs.1178/dados/ultimos/1?formato=json"
+    return getJson(url)
 
-print("debugando")
-#print(get_historico_bitcoin())
-#print(get_cotacoes())
-print(get_taxa_selic())
-print("teste debug api LINHA 2")
+cotacoes = getCotacoes()
+historicoBit = getHistoricoBitcoin()
+selic = getTaxaSelic()
+def formatarCotacoes(cotacoes):
+    listaCotacoes = []
+    for item in cotacoes:
+        listaCotacoes.append({
+            'Alta': float(item['high']),
+            'Baixa': float(item['low']),
+            'Fechamento': float(item['bid'])
+        })
+    return list(reversed(listaCotacoes)) # para mostrar da mais antiga à mais recente
+
+historicoBTC = []
+
+for item in getHistoricoBitcoin():
+    registro = {
+        "data_hora": (datetime.fromtimestamp(int(item["timestamp"]))).strftime("%d/%m/%Y %H:%M:%S"),
+        "high": float(item["high"]),
+        "low": float(item["low"]),
+        "bid": float(item["bid"]),
+        "ask": float(item["ask"])
+    }
+    historicoBTC.append(registro)
+
+print(historicoBTC)
--- utils/excel_writer.py
+++ utils/excel_writer.py
@@ -3,3 +3,50 @@ from openpyxl.styles import Font, Alignment, NamedStyle
 from openpyxl.utils import get_column_letter
 from datetime import datetime
 
+def criarArquivo (cotacoes, historico, selic, log):
+    arquivo = Workbook()
+
+    def formatarAba(aba, dados):
+        fonteNegrito = Font(bold=True)
+        numeroEstilo = NamedStyle(name="numeroEstilo", number_format="0.00") #Variavel para setar pontos flutuantes em ate duas casas
+
+        #Criando os cabeçalhos dos arquivos
+        for indexColuna, coluna in enumerate(dados[0].keys, start=1):
+            cell = aba.cell(roll = 1, column = indexColuna, value = coluna)
+            cell.font = fonteNegrito
+            cell.alignment = Alignment(horizontal = "center")
+
+        #Criando as linhas de dados
+        for indexLinha, itens in enumerate(dados, start=2):
+            for indexColuna, valor in enumerate(itens.values(), start=1):
+                cell = aba.cell(row = indexLinha, collunm = indexColuna, value = valor)
+                if isinstance(valor, (int, float)):
+                    cell.style = numeroEstilo
+
+        #Ajustando a largura das colunas
+        for indexColuna in range(1, len(dados[0])+1):
+            letraColuna = get_column_letter(indexColuna)
+            aba.column_dimensions[letraColuna].auto_size = True
+
+
+    ws_resumo = arquivo.active
+    ws_resumo.title = "Resumo Atual"
+    formatarAba(ws_resumo, cotacoes)
+
+    # Aba 2: Histórico 30 dias (apenas USD por enquanto, ou adapte)
+    ws_hist = arquivo.create_sheet("Histórico 30 dias")
+    formatarAba(ws_hist, historico)
+
+    # Aba 3: Taxas de Juros
+    ws_selic = arquivo.create_sheet("Taxas de Juros")
+    formatarAba(ws_selic, selic)
+
+    # Aba 4: Log de Execução
+    ws_log = arquivo.create_sheet("Log de Execução")
+    formatarAba(ws_log, log)
+
+    # Salvar o arquivo
+    data_hora = datetime.now().strftime("%Y%m%d_%H%M%S")
+    arquivo.save(f"relatorio_{data_hora}.xlsx")
+
+criarArquivo()
