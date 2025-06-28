7e243fbe7ed7ee064af9ceecb4abdda782c75abd
Josef Ian <josefian2016@gmail.com>
2025-06-28 20:00:47 -0300

    mudando registro de link da API do banco central

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 COTACOES_URL = "https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL"
 HISTORICO_URL = "https://economia.awesomeapi.com.br/json/daily/BTC-BRL/30"
 
-SELIC_URL = "https://api.bcb.gov.br/dados/serie/bcdata.sgs.4189/dados?formato=json"
+SELIC_URL = "https://api.bcb.gov.br/dados/serie/bcdata.sgs.1178/dados/ultimos/1?formato=json"
