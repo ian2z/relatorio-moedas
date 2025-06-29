f4f903f8f7271e70925901158cd88ff9db3a3646
Josef Ian <josefian2016@gmail.com>
2025-06-29 11:49:52 -0300

    criando funções de formatação das requisições

--- services/api_client.py
+++ services/api_client.py
@@ -22,29 +22,43 @@ def getTaxaSelic():
     url = "https://api.bcb.gov.br/dados/serie/bcdata.sgs.1178/dados/ultimos/1?formato=json"
     return getJson(url)
 
-cotacoes = getCotacoes()
-historicoBit = getHistoricoBitcoin()
-selic = getTaxaSelic()
-def formatarCotacoes(cotacoes):
-    listaCotacoes = []
-    for item in cotacoes:
-        listaCotacoes.append({
-            'Alta': float(item['high']),
-            'Baixa': float(item['low']),
-            'Fechamento': float(item['bid'])
-        })
-    return list(reversed(listaCotacoes)) # para mostrar da mais antiga à mais recente
+#Criando funções que retornam os arquivos .json formatados em arrays
+def formatarCotacoes(json_cotacoes):
+    cotacoes = []
+    for chave, dados in json_cotacoes.items():
+        registro = {
+            "moeda": dados["code"],
+            "nome": dados["name"],
+            "data_hora": datetime.fromtimestamp(int(dados["timestamp"])).strftime("%d/%m/%Y %H:%M:%S"),
+            "alta": float(dados["high"]),
+            "baixa": float(dados["low"]),
+            "compra": float(dados["bid"]),
+            "venda": float(dados["ask"]),
+            "variacao (%)": float(dados["pctChange"])
+        }
+        cotacoes.append(registro)
+    return cotacoes
 
-historicoBTC = []
+def formatarHistorico(json_historico):
+    historicoBTC = []
+    for item in json_historico:
+        registro = {
+            "data_hora": (datetime.fromtimestamp(int(item["timestamp"]))).strftime("%d/%m/%Y %H:%M:%S"),
+            "high": float(item["high"]),
+            "low": float(item["low"]),
+            "bid": float(item["bid"]),
+            "ask": float(item["ask"])
+        }
+        historicoBTC.append(registro)
+    return historicoBTC
 
-for item in getHistoricoBitcoin():
-    registro = {
-        "data_hora": (datetime.fromtimestamp(int(item["timestamp"]))).strftime("%d/%m/%Y %H:%M:%S"),
-        "high": float(item["high"]),
-        "low": float(item["low"]),
-        "bid": float(item["bid"]),
-        "ask": float(item["ask"])
-    }
-    historicoBTC.append(registro)
-
-print(historicoBTC)
+#esta API já retorna um array com os dados fornecidos, e usaremos todos eles, portanto, não há necessidade dela, mas por motivo de um futuro tratamento ou troca de API, ela foi criada.
+def formatarSelic(json_selic):
+    historicoSelic = []
+    for item in json_selic:
+        registro = {
+            "data": str(item["data"]),
+            "valor": float(item["valor"])
+        }
+    historicoSelic.append(registro)
+    return historicoSelic
